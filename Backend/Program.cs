bade8d0c288f3e857975539ed7446c7552348b84
Hekku2 <hekku2@gmail.com>
2018-04-23 19:32:52 +0300

    #1 Quick and  dirty address configuration

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,19 +1,27 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace Backend
 {
     public class Program
     {
+        public static IConfiguration Configuration { get; set; }
+
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+            BuildWebHost(args, Configuration["ServerAddress"]).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
+        public static IWebHost BuildWebHost(string[] args, string address) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls("http://localhost:5000")
+                .UseUrls(address)
                 .Build();
     }
 }
